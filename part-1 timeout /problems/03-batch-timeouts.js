2700c9c86f555cb74c7ee677168cac94ed59f61d
Aman Kaur <amanpreet_k32@yahoo.com>
2022-05-24 20:58:48 -0700

    problem 3

--- part-1 timeout /problems/03-batch-timeouts.js
+++ part-1 timeout /problems/03-batch-timeouts.js
@@ -5,10 +5,10 @@ callback in the array with its corresponding delay. For example, the
 callback at index 0 should be set with the delay at index 0,
 the callback at index 1 should be set with the delay at index 1, and so on.
 The `batchTimeouts` function should return an array containing the Timeout
-objects for each timeout that was set. You may assume that both array arguments 
+objects for each timeout that was set. You may assume that both array arguments
 have the same length.
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below to confirm the correct behavior.
 
 Note: The test specs for this problem are valid for iterative solutions. If you
@@ -23,8 +23,8 @@ const shout = () => console.log('WHAT?');
 const tasks = [sayHello, sayGoodbye, shout];
 const delays = [500, 200, 900];
 
-const timeoutObjs = batchTimeouts(tasks, delays); 
-// should print: 
+console.log(const timeoutObjs = batchTimeouts(tasks, delays));
+// should print:
 //  'bye' after 200 ms
 //  'hi' after 500 ms
 //  'WHAT?' after 900 ms
@@ -33,7 +33,13 @@ console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 ***********************************************************************/
 
 function batchTimeouts(callbacks, delays) {
-  // Your code here
+  let results = []
+  for (let i = 0; i < callbacks.length; i++) {
+    let timer = setTimeout(callbacks[i], delays[i])
+    results.push(timer)
+  }
+  return results;
+
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
